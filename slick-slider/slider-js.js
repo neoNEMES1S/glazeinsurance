48f2f684143036271261d37477a901678988a62a
Ankit Johri <ansu699@gmail.com>
2026-03-05 13:02:04 +0530

    feat: Add initial website implementation with all core pages, styles, scripts, and media assets.

---
+++ slick-slider/slider-js.js
@@ -0,0 +1,70 @@
+﻿//---- SLIDER---->
+$('.slider-review').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: false,
+    asNavFor: '.slider-nav',
+    autoplay: false,
+    infinite: false,
+    responsive: [  
+    {
+        breakpoint: 767,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false
+        }
+    }
+    ]
+});
+
+
+
+
+$('.slider-partner').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: false,
+    asNavFor: '.slider-nav',
+    autoplay: true,
+    dots: false,
+    speed: 3000,
+    autoplaySpeed: 0,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
+    infinite: true,
+    responsive: [
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        }
+    ]
+});
+
+$('.slider-partners').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: false,
+    asNavFor: '.slider-nav',
+    autoplaySpeed: 800,
+    autoplay: true,
+    infinite: true,
+    responsive: [
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        }
+    ]
+});
